b2deff7e506941f624b46475c79c560a4eb0464b
I507145 <jingwei.li@sap.com>
2020-04-16 09:54:56 +0800

    Adjust

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# api-hitter
+# hitter
 Performance test tool built with Golang.
+
+## Install
+### Prerequisite
+- go 1.11.4 or higher version: https://golang.org/
+- godep: https://github.com/tools/godep
+
+### Step
+Pull this repository and run following commands in the root directory:
+```shell script
+godep restore
+go build
+```
+It should be error free and now you could see there is an executable file in the root directory.
+## Usage
+```shell script
+Usage: hitter [-hncmHbfs] [-a apiUrl]
+
+Options:
+  -H value
+        request headers, format: name=value
+  -a string
+        api url
+  -b string
+        request body
+  -c int
+        maximum go-channel number to limit concurrency (default 1)
+  -f string
+        read a file as request body, won't work if -b is provided
+  -h    help
+  -m string
+        request method (default "GET")
+  -n int
+        total requests number (default 1)
+  -s    don't save log
+```
--- main.go
+++ main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"flag"
 	"fmt"
+	"github.com/gookit/color"
+	"golang.org/x/net/http/httpguts"
 	"io/ioutil"
 	"net"
 	"net/http"
 	"os"
-	"strconv"
+	"strings"
 	"sync"
 	"time"
 )
@@ -19,6 +23,12 @@ var (
 
 	lock   = sync.Mutex{}
 	logBuf = NewMutexBuffer()
+
+	usage = `Version: 0.0.1-SNAPSHOT
+Usage: hitter [-hncmHbfs] [-a apiUrl]
+
+Options:
+`
 )
 
 // progress animation
@@ -27,10 +37,43 @@ var (
 	crI = 0
 )
 
+type Headers map[string]string
+
+func (h Headers) String() string {
+	tmp := make([]string, len(h))
+
+	i := 0
+	for name, value := range h {
+		tmp[i] = fmt.Sprintf("%s=%s", name, value)
+		i++
+	}
+	return strings.Join(tmp, ",")
+}
+
+func (h Headers) Set(value string) error {
+	i := strings.IndexRune(value, '=')
+	if i <= 0 {
+		return fmt.Errorf("invalid header")
+	}
+	h[value[:i]] = value[i+1:]
+	return nil
+}
+
+type RequestParams struct {
+	url     string
+	method  string
+	headers Headers
+	body    []byte
+}
+
 func main() {
-	reqNum, concurrency, url := parseArguments()
+	notSaveLog, reqNum, concurrency, requestParams := parseArguments()
 	totalTaskNum = reqNum
 
+	fmt.Println("url:", requestParams.url)
+	fmt.Println("method:", requestParams.method)
+	fmt.Println("headers:", requestParams.headers)
+	fmt.Println("body:", len(requestParams.body), "bytes")
 	fmt.Println("request num:", reqNum)
 	fmt.Println("concurrency:", concurrency)
 
@@ -53,64 +96,130 @@ func main() {
 	ex := CreateExecutor(concurrency)
 	fmt.Println("----------------")
 
-	start := time.Now()
+	start := time.Now().UnixNano()
 	for i := 0; i < reqNum; i++ {
-		_ = ex.Execute(makeRequest, client, url)
+		_ = ex.Execute(makeRequest, client, requestParams)
 	}
 	ex.Stop()
+	timeUsage := float64(time.Now().UnixNano()-start) / 1e9
 
 	// print statistics
-	timeUsage := time.Since(start).Seconds()
 	printStatistics(timeUsage)
 
 	// save failed log
-	if err := ioutil.WriteFile("failed.log", logBuf.b.Bytes(), 0777); nil != err {
-		fmt.Println(err)
+	if !notSaveLog {
+		if err := ioutil.WriteFile("failed.log", logBuf.b.Bytes(), 0777); nil != err {
+			fmt.Println(err)
+		}
 	}
 }
 
-func parseArguments() (reqNum int, concurrency int, url string) {
-	if os.Args == nil || len(os.Args) != 4 {
-		fmt.Println("usage: reqNum concurrency apiUrl")
-		return
+func parseArguments() (bool, int, int, RequestParams) {
+	help := flag.Bool("h", false, "help")
+	notSaveLog := flag.Bool("s", false, "don't save log")
+
+	reqNum := flag.Int("n", 1, "total requests number")
+	concurrency := flag.Int("c", 1, "maximum go-channel number to limit concurrency")
+
+	url := flag.String("a", "", "api url")
+	method := flag.String("m", "GET", "request method")
+	headers := Headers{}
+	flag.Var(&headers, "H", "request headers, format: name=value")
+	bodyString := flag.String("b", "", "request body")
+	file := flag.String("f", "", "read a file as request body, won't work if -b is provided")
+
+	flag.Usage = func() {
+		_, _ = fmt.Fprintf(os.Stderr, usage)
+		flag.PrintDefaults()
 	}
 
-	// read command arguments
-	var err error
-	reqNum, err = strconv.Atoi(os.Args[1])
-	concurrency, err = strconv.Atoi(os.Args[2])
-	url = os.Args[3]
-	if nil != err {
-		panic(err)
+	// parse
+	flag.Parse()
+
+	if *help {
+		flag.Usage()
+		os.Exit(0)
 	}
 
-	if reqNum <= 0 {
-		panic(fmt.Errorf("reqNum must positive"))
-	}
-	if concurrency <= 0 {
-		panic(fmt.Errorf("concurrency must positive"))
+	if *url == "" {
+		color.Red.Println("api url missing")
+		os.Exit(1)
 	}
 
-	return
+	// check if method is valid
+	if !validMethod(*method) {
+		color.Red.Printf("invalid method %q\n", *method)
+		os.Exit(1)
+	}
+
+	var body []byte
+	if *bodyString == "" && *file != "" {
+		// try to read file as body
+		var err error
+		body, err = ioutil.ReadFile(*file)
+		if err != nil {
+			color.Red.Println(err)
+			os.Exit(1)
+		}
+	} else {
+		body = []byte(*bodyString)
+	}
+
+	return *notSaveLog, *reqNum, *concurrency,
+		RequestParams{
+			url:     *url,
+			method:  *method,
+			headers: headers,
+			body:    body,
+		}
+}
+
+func validMethod(method string) bool {
+	/*
+	     Method         = "OPTIONS"                ; Section 9.2
+	                    | "GET"                    ; Section 9.3
+	                    | "HEAD"                   ; Section 9.4
+	                    | "POST"                   ; Section 9.5
+	                    | "PUT"                    ; Section 9.6
+	                    | "DELETE"                 ; Section 9.7
+	                    | "TRACE"                  ; Section 9.8
+	                    | "CONNECT"                ; Section 9.9
+	                    | extension-method
+	   extension-method = token
+	     token          = 1*<any CHAR except CTLs or separators>
+	*/
+	return len(method) > 0 && strings.IndexFunc(method, isNotToken) == -1
+}
+
+func isNotToken(r rune) bool {
+	return !httpguts.IsTokenRune(r)
 }
 
 func makeRequest(args ...interface{}) {
 	client := args[0].(*http.Client)
-	url := args[1].(string)
+	requestParams := args[1].(RequestParams)
 
-	// FIXME: provide as command arguments
+	var timeUsage float64
+
 	var rep *http.Response
-	req, err := http.NewRequest("GET", url, nil)
+	var bodyReader = bytes.NewReader(requestParams.body)
+	req, err := http.NewRequest(requestParams.method, requestParams.url, bodyReader)
 	if err == nil {
-		// FIXME: provide as command arguments
-		//req.Header.Add("Authorization", "Basic U0hFTkpJQU4wOlNqOTQwNTE1")
+		// add headers
+		for name, value := range requestParams.headers {
+			req.Header.Add(name, value)
+		}
 
+		// do request
+		start := time.Now()
 		rep, err = client.Do(req)
+		timeUsage = time.Since(start).Seconds()
+
 	}
 
 	// submit execution result
 	if err == nil && rep.StatusCode == 200 {
-		submit(true)
+		submit(timeUsage, true)
 	} else {
 		// log error info
 		var repInfo interface{} = nil
@@ -119,11 +228,11 @@ func makeRequest(args ...interface{}) {
 		}
 		_, _ = fmt.Fprintf(logBuf, "[%v] %v\n", repInfo, err)
 
-		submit(false)
+		submit(timeUsage, false)
 	}
 }
 
-func submit(success bool) {
+func submit(timeUsage float64, success bool) {
 	lock.Lock()
 	defer lock.Unlock()
 
@@ -138,13 +247,17 @@ func submit(success bool) {
 }
 
 func printProgress() {
-	fmt.Printf("%c hitting %.2f%%\r", cr[crI], float32(finished)*float32(100)/float32(totalTaskNum))
+	color.Green.Printf("%c hitting %.2f%%\r", cr[crI], float32(finished)*float32(100)/float32(totalTaskNum))
 	crI = (crI + 1) % 4
 }
 
 func printStatistics(timeUsage float64) {
 	fmt.Printf("total time: %.6f sec\n", timeUsage)
-	fmt.Printf("qps: %.6f\n", float64(totalTaskNum)/timeUsage)
+	if timeUsage == 0 {
+		fmt.Printf("qps: %.6f\n", float64(0))
+	} else {
+		fmt.Printf("qps: %.6f\n", float64(totalTaskNum)/timeUsage)
+	}
 	fmt.Printf("total: %d\n", totalTaskNum)
 	fmt.Printf("failed: %d\n", failed)
 }
